80331be70fdaf0365fc70efee893d883e8259408
georgie sandra <dzouchesandra@gmail.com>
2026-03-31 11:09:01 +0200

    Ajout de l'interface utilisateur

--- Cesar.py
+++ Cesar.py
@@ -15,3 +15,18 @@ def cesar_chiffrer(message, cle):
 def cesar_dechiffrer(message, cle):
     # Déchiffrer, c'est chiffrer avec l'inverse de la clé
     return cesar_chiffrer(message, -cle)
+
+
+# --- Bonus : Interface Utilisateur ---
+if __name__ == "__main__":
+    print("--- Code César ---")
+    choix = input("1: Chiffrer | 2: Déchiffrer : ")
+    message = input("Message : ")
+    cle = int(input("Clé (nombre entier) : "))
+
+    if choix == "1":
+        print("Résultat chiffré :", cesar_chiffrer(message, cle))
+    elif choix == "2":
+        print("Résultat déchiffré :", cesar_dechiffrer(message, cle))
+    else:
+        print("Choix invalide.")
